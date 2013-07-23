a23b2da48aaa746ecd415c3c016054bc791ab2c2
Dennis Ruhe <dennis.r.ruhe@gmail.com>
2013-07-23 15:13:11 +0200

    More setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link href="media/vendor/prettify/prettify.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="media/vendor/prettify/run_prettify.js"></script>
+	</head>
+	<body>
+		<pre class="prettyprint lang-css">
+			div.some-css { 
+				width: test; 
+			}
+		</pre>
+
+		<pre class="prettyprint lang-sql">
+			SELECT * FROM some_table
+		</pre>
+
+		<pre class="prettyprint lang-js">
+			$.each(dummy, function() { alert('hello'); });
+		</pre>
+
+		<pre class="prettyprint lang-php">
+<?php echo htmlentities(file_get_contents('test.php')); ?>
+		</pre>
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,18 @@
+<?php echo 'Test'; ?>
+
+<?php phpinfo(); ?>
+
+<?php
+function hello_world() 
+{
+	echo "Hello world!";
+}
+
+// Comment
+/**
+ * Supercomment
+ */
+# ignored text
+
+hello_world();
+?>
