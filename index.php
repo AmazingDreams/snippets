ee8044438e01f2b74546354434a9164a04867328
Dennis Ruhe <dennis.r.ruhe@gmail.com>
2013-07-23 15:40:53 +0200

    Added file list

--- index.php
+++ index.php
@@ -19,14 +19,25 @@ $ext      = preg_replace('/^.*\.([^.]+)$/D', '$1', $file);
 		<script type="text/javascript" src="static/js/vendor/google-code-prettify/run_prettify.js"></script>
 	</head>
 	<body>
-		<h1><?php echo $filename; ?></h1>
+		<div class="files">
+			<?php $directory = opendir("snippets"); ?>
+			<?php while($iterator = readdir($directory)): ?>
+				<?php if(strpos($iterator, '.') == 0) continue;  ?>
 
-		<?php if(file_exists($file)): ?>
-			<pre class="prettyprint lang-<?php echo $ext; ?> linenums">
+				<a href="<?php echo $config['url'] .'?file='. $iterator; ?>"><?php echo $iterator; ?></a>
+			<?php endwhile; ?>
+		</div>
+
+		<div class="selected-file">
+			<h1><?php echo $filename; ?></h1>
+
+			<?php if(file_exists($file)): ?>
+				<pre class="prettyprint lang-<?php echo $ext; ?> linenums">
 <?php echo htmlentities(file_get_contents("$file")); ?>
-			</pre>
-		<?php else: ?>
-			<h3>File not found :( </h3>
-		<?php endif; ?>
+				</pre>
+			<?php else: ?>
+				<h3>File not found :( </h3>
+			<?php endif; ?>
+		</div>
 	</body>
 </html>
