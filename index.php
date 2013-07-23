be9e0a9f43ded1d7f2e09076f8179091cde14745
T. Zengerink <t.zengerink@gmail.com>
2013-07-23 15:43:52 +0200

    modified configuration

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
 <?php
-/**
- * Configuration
- */
-define('DOCROOT', $_SERVER['DOCUMENT_ROOT']);
+	// Configuration
+	//define('DOCROOT', $_SERVER['DOCUMENT_ROOT'].'/developers/teun/snippets');
+	include('config.php');
 
-$config = require_once 'config.php';
-
-// Get filename
-$filename = $_GET['file'];
-$file     = "{$config['install_dir']}/snippets/$filename";
-$ext      = preg_replace('/^.*\.([^.]+)$/D', '$1', $file);
+	// Get filename
+	$filename = $_GET['file'];
+	$file     = $config['root'].'/snippets/'.$filename;
+	$ext      = preg_replace('/^.*\.([^.]+)$/D', '$1', $file);
 ?>
 
+<!DOCTYPE html>
 <html>
 	<head>
 		<link href="static/js/vendor/google-code-prettify/prettify.css" type="text/css" rel="stylesheet" />
@@ -22,9 +19,7 @@ $ext      = preg_replace('/^.*\.([^.]+)$/D', '$1', $file);
 		<h1><?php echo $filename; ?></h1>
 
 		<?php if(file_exists($file)): ?>
-			<pre class="prettyprint lang-<?php echo $ext; ?> linenums">
-<?php echo htmlentities(file_get_contents("$file")); ?>
-			</pre>
+			<pre class="prettyprint lang-<?php echo $ext; ?> linenums"><?php echo htmlentities(file_get_contents($file)); ?></pre>
 		<?php else: ?>
 			<h3>File not found :( </h3>
 		<?php endif; ?>
