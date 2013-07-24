520a108424027e9eea3a86c98e40f818a46f0cd8
Dennis Ruhe <dennis.r.ruhe@gmail.com>
2013-07-24 11:15:43 +0200

    Increased security

--- application/sanitize.php
+++ application/sanitize.php
@@ -1,5 +1,5 @@
 <?php
 	function sanitize_filename($string)
 	{
-		return preg_replace('~\s?([.]+[/|\\\]+)~', '', $string);
+		return preg_replace('~\s?(([.]+)?(^[a-zA-Z0-9]?[/|\\\]+))~', '', $string);
 	}
